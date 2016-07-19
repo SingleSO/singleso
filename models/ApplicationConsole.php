d08ee2781552e6dcbb5ab041032416889725032e
Alexander O'Mara <me@alexomara.com>
2016-07-18 22:49:06 -0400

    Make console work before initial install.

--- models/ApplicationConsole.php
+++ models/ApplicationConsole.php
@@ -5,24 +5,31 @@ namespace app\models;
 use Yii;
 use app\models\Config;
 use yii\console\Application;
+use yii\db\Exception as DBException;
 
 class ApplicationConsole extends Application {
 	public function __construct($config) {
 		parent::__construct($config);
 
-		// Set name from database if not set by config file.
-		if (!isset($config['name'])) {
-			if (($name = Config::setting('application.name'))) {
-				$this->name = $name;
+		// If database if unavailable skip settings (initital install).
+		try {
+			// Set name from database if not set by config file.
+			if (!isset($config['name'])) {
+				if (($name = Config::setting('application.name'))) {
+					$this->name = $name;
+				}
+			}
+
+			// Set email from database if not set by config file.
+			if (!isset(Yii::$app->params['adminEmail'])) {
+				Yii::$app->params['adminEmail'] = null;
+				if (($email = Config::setting('application.admin.email'))) {
+					Yii::$app->params['adminEmail'] = $email;
+				}
 			}
 		}
-
-		// Set email from database if not set by config file.
-		if (!isset(Yii::$app->params['adminEmail'])) {
-			Yii::$app->params['adminEmail'] = null;
-			if (($email = Config::setting('application.admin.email'))) {
-				Yii::$app->params['adminEmail'] = $email;
-			}
+		catch (DBException $e) {
+			// Do nothing.
 		}
 	}
 }
